23c011b904109aaf128b4e9ddf4045d8db85afff
Kevin Lanzon <kevinlanzon@gmail.com>
2015-02-07 12:35:52 +0000

    added all plane specs and passed

--- lib/plane.rb
+++ lib/plane.rb
@@ -4,8 +4,20 @@ class Plane
 		@flying = true
 	end
 
+  def status
+    @flying
+  end
+
 	def flying?
 		@flying
 	end
+
+  def take_off
+    @flying
+  end
+
+  def land?
+    @flying
+  end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -5,7 +5,22 @@ describe Plane do
 	let(:plane) { Plane.new }
 
 	it 'has a flying status when created' do
-		expect(plane.flying?).to be(true)
+		expect(plane.status).to eq(true)
 	end
+
+  it 'has a flying status when in the air' do
+    plane.take_off
+    expect(plane.flying?).to be(true)
+  end
+
+  it 'can take off' do
+    plane.take_off
+    expect(plane.take_off).to be(true)
+  end
+
+  it 'can land' do
+    plane.land?
+    expect(plane.land?).to be(true)
+  end
 
 end
