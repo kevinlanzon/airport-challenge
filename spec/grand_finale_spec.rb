7a7c9f707fd6b202404e4b41047b0bd43541aef2
Kevin Lanzon <kevinlanzon@gmail.com>
2015-02-08 14:03:41 +0000

    added grand finale spec

---
+++ spec/grand_finale_spec.rb
@@ -0,0 +1,17 @@
+require 'airport'
+require 'plane'
+
+describe 'The Grand Finale Spec (last spec)' do
+
+  it 'all planes can land and all planes can take off' do
+  end
+
+end
+
+
+
+# grand final
+# Given 6 planes, each plane must land. When the airport is full, every plane must take off again.
+# Be careful of the weather, it could be stormy!
+# Check when all the planes have landed that they have the right status "landed"
+# Once all the planes are in the air again, check that they have the status of flying!
